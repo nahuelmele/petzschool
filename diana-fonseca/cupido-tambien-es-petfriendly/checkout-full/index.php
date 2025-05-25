38aae1f6c7196deeab7c019d8a7341b95943a495
Nahuel Mele <NMele@dacgroup.com>
2025-05-25 19:51:36 -0300

    changes

---
+++ diana-fonseca/cupido-tambien-es-petfriendly/checkout-full/index.php
@@ -0,0 +1,7 @@
+<meta property="og:url" content="https://petzschool.com/diana-fonseca/cupido-tambien-es-petfriendly/" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Cupido también es Petfrienly de Diana Fonseca - Petz School" />
+<meta property="og:image" content="https://petzschool.com/img/landings/cupido-tambien-es-petfriendly/compartir-cupido-tambien-es-petfriendly.jpg" />
+<script>
+  window.location.href = 'https://go.hotmart.com/R78911738N?ap=3551';
+</script>
